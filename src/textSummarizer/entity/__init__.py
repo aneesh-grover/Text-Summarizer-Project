0c0a8d818aadbf0ac1a0308e9b908348252a451f
aneesh-grover <Aneesh1817.be20@chitkara.edu.in>
2024-06-10 23:02:06 +0530

    data ingestion notebook added

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from pathlib import Path
+import sys
+import os
+
+# Add the src directory to the Python path
+current_working_directory = os.getcwd()
+src_directory = os.path.join(current_working_directory, '/Users/aneeshgrover/PROJECTS/Text-Summarizer-Project/src')
+sys.path.insert(0, src_directory)
+
+from textSummarizer.utils.common import read_yaml, create_directories
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
